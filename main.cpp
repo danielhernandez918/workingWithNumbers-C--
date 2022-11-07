95b664a5e5835e36aeb81177e741e100382e5801
Daniel Hernandez <danielhernandez918@gmail.com>
2022-11-07 15:45:52 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main () 
+{
+    cout<< 40 + 7 << endl;
+    int wnum =5;
+    double dnum = 5.5;
+    wnum++;
+    cout << wnum << endl;
+    cout << pow(5, 2)<< endl;
+    cout << sqrt(36)<< endl;
+    cout << round(5.7);
+    return 0;
+}
